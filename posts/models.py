8a832e167673967b819106ae0c437f90f85b77b6
Michel Atieno <mishqamish@gmail.com>
2018-10-11 14:33:04 +0300

    Added basic structure and able to display views

---
+++ posts/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
+
+# Create your models here.
+class Profile(models.Model):
+    profile_photo = models.ImageField(upload_to ='prof_pictures/')
+    bio = HTMLField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True, default="")
+
+
+
+
+class Image(models.Model):
+    photo = models.ImageField(upload_to ='prof_pictures/')
+    image_name = models.CharField(max_length = 50)
+    image_caption =  models.CharField(max_length = 50)
+    user_profile = models.ForeignKey(User, on_delete=models.CASCADE, default="")
