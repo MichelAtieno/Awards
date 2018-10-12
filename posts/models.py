b61a9e561bd6e3e1f3d8556844f8f0da2aab64e5
Michel Atieno <mishqamish@gmail.com>
2018-10-12 16:25:00 +0300

    Able to navigate to users profiles who has logged in

--- posts/models.py
+++ posts/models.py
@@ -30,31 +30,31 @@ class Profile(models.Model):
 
 
 
-class Image(models.Model):
+class Project(models.Model):
     photo = models.ImageField(upload_to ='prof_pictures/')
     project_name = models.CharField(max_length = 100)
     project_caption =  models.CharField(max_length = 100)
     user_profile = models.ForeignKey(User, on_delete=models.CASCADE, default="")
 
-    def save_image(self):
+    def save_project(self):
         self.save()
 
-    def delete_image(self):
+    def delete_project(self):
         self.delete()
     
 
     @classmethod
-    def get_image(cls, id):
-        image = Image.objects.get(pk=id)
-        return image
+    def get_project(cls, id):
+        project = Project.objects.get(pk=id)
+        return project
     
     @classmethod
     def get_images(cls):
-        images = Image.objects.all()
-        return images
+        projects = Project.objects.all()
+        return projects
 
     @classmethod
     def get_profile_image(cls,profile):
-        images = Image.objects.filter(user_profile__pk=profile)
-        return images
+        projects = Project.objects.filter(user_profile__pk=profile)
+        return projects
 
--- posts/views.py
+++ posts/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from .forms import RegistrationForm
-from .models import Profile,Image
+from .models import Profile,Project
 
 # Create your views here.
 def home(request):
@@ -24,10 +24,10 @@ def register(request):
 
 def profile(request, username):
     profile = User.objects.get(username=username)
-    # try:
-    profile_info = Profile.get_profile(profile.id)
-    # # except:
-    # profile_info = Profile.filter_by_id(profile.id)
-    images = Image.get_profile_image(profile.id)
+    try:
+        profile_info = Profile.get_profile(profile.id)
+    except:
+        profile_info = Profile.filter_by_id(profile.id)
+    projects = Project.get_profile_image(profile.id)
     title = f'@{profile.username}'
-    return render(request, 'profile/profile.html', {'title':title, 'profile':profile, 'profile_info':profile_info, 'images':images})  
+    return render(request, 'profile/profile.html', {'title':title, 'profile':profile, 'profile_info':profile_info, 'projects':projects})  
