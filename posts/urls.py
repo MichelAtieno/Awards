7f73eb2807448287e082dcb8c4a3793a66ac8c6a
Michel Atieno <mishqamish@gmail.com>
2018-10-15 10:04:12 +0300

    Able to GET data from project

--- posts/urls.py
+++ posts/urls.py
@@ -10,7 +10,8 @@ urlpatterns=[
     url(r'^review/(?P<project_id>\d+)',views.project_review ,name='project_review'),
     url(r'^accounts/edit', views.edit_profile, name='edit_profile'),
     url(r'^search/', views.search_profile, name='search'),
-    url(r'^api/profile/$', views.ProfileList.as_view())
+    url(r'^api/profile/$', views.ProfileList.as_view()),
+    url(r'^api/project/$', views.ProjectList.as_view())
 ]
 
 if settings.DEBUG:
