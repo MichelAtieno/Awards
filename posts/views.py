9f44e1c0f3c7a40e400d53c5d29827d8a74442bb
Michel Atieno <mishqamish@gmail.com>
2018-10-15 10:20:42 +0300

    Able to POST data from both project and profile

--- posts/views.py
+++ posts/views.py
@@ -6,7 +6,9 @@ from .forms import RegistrationForm, ProjectForm, ReviewForm, ProfileForm
 from .models import Profile,Project,Reviews
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import status
 from .serializer import ProfileSerializer, ProjectSerializer
+
 # Create your views here.
 
 def home(request):
@@ -110,11 +112,25 @@ class ProfileList(APIView):
         serializers = ProfileSerializer(all_profiles,many=True)
         return Response(serializers.data)
 
+    def post(self, request, format=None):
+        serializers = ProfileSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
 class ProjectList(APIView):
     def get(self, request, format=None):
         all_projects = Project.objects.all()
         serializers = ProjectSerializer(all_projects,many=True)
         return Response(serializers.data)
+
+    def post(self, request, format=None):
+        serializers = ProjectSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
     
